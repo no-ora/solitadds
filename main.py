e2c189c46724e01f78a19453a82e0ff6d8f01009
no-ora <nooraevainio@gmail.com>
2016-11-10 17:44:19 +0200

    save plots as images

--- main.py
+++ main.py
@@ -24,10 +24,11 @@ def parse_args():
     python main.py --input-file-operative ../data/small/some-applications-operative-pub-20161031.csv --input-file-usage ../data/small/some-lupapiste-usage-pub-20161031.csv --output-file-applications ../target/application-summary.csv --output-file-users ../target/user-summary.csv
     """
     parser = argparse.ArgumentParser(description='SOLITADDS analysis')
-    parser.add_argument('-io', '--input-file-operative', help='Input CSV file for operative data', required = False, default = os.getcwd() + "/test-data/all-applications-operative-pub-20161031.csv")
+    parser.add_argument('-io', '--input-file-operative', help='Input CSV file for operative data', required = False, default = os.getcwd() + "/test-data/some-applications-operative-pub-20161031.csv")
     parser.add_argument('-iu', '--input-file-usage', help='Input CSV file for usage data', required = False, default = os.getcwd() + "/test-data/some-lupapiste-usage-pub-20161031.csv")
-    parser.add_argument('-oa', '--output-file-applications', help='Output CSV file for applications', required = False, default = os.getcwd() + "summary-applications.csv")
-    parser.add_argument('-ou', '--output-file-users', help='Output CSV file for users', required=False, default = os.getcwd() + "summary-users.csv")
+    parser.add_argument('-oa', '--output-file-applications', help='Output CSV file for applications', required = False, default = os.getcwd() + "/summary-applications.csv")
+    parser.add_argument('-ou', '--output-file-users', help='Output CSV file for users', required=False, default = os.getcwd() + "/summary-users.csv")
+    parser.add_argument('-outputimages', '--output-images-folder', help='Output folder for plotimages', required=False, default = os.getcwd() + "/plots")
     args = vars(parser.parse_args())
     return args
 
@@ -39,6 +40,7 @@ if __name__ == "__main__":
     input_file_usage = args['input_file_usage']
     output_file_applications = args['output_file_applications']
     output_file_users = args['output_file_users']
+    output_images_folder = args['output_images_folder']
 
     analysis_start_time = datetime.datetime.now()
 
@@ -58,19 +60,29 @@ if __name__ == "__main__":
     print("Analysis took {} seconds".format(datetime.datetime.now() - analysis_start_time))
 
     odf['createdMonth'] = odf['createdDate'].dt.month
+    
+    # Create folder for images
+    if not os.path.exists(output_images_folder):
+        os.makedirs(output_images_folder)
+    
+    # Barchart for applications by month
     plt.figure(1)
-    plt.title("Hakemukset kuukausittain")
+    plt.title("Kaikki")
     plt.xlabel("Kuukausi")
     plt.ylabel("Hakemusten maara")
     analyze.applications_by_month(odf)
-    plt.figure(2)
-    plt.title("Pientalohakemukset kuukausittain")
-    plt.xlabel("Kuukausi")
-    plt.ylabel("Hakemusten maara")
-    analyze.applications_by_month_by_action(odf, 'pientalo')
-    plt.figure(3)
-    plt.title("Maalampohakemukset kuukausittain")
-    plt.xlabel("Kuukausi")
-    plt.ylabel("Hakemusten maara")
-    analyze.applications_by_month_by_action(odf, 'maalampo')
-    plt.show()
+    plt.savefig(output_images_folder + "/kaikki_kuukausittain.png")
+    
+    
+    operationIds = odf['operationId'].unique()
+    counter = 2
+    for operationId in operationIds:
+        if isinstance(operationId, basestring):
+            plt.figure(counter)
+            counter = counter + 1
+            plt.title(operationId)
+            plt.xlabel("Kuukausi")
+            plt.ylabel("Hakemusten maara")
+            analyze.applications_by_month_by_action(odf, operationId)
+            plt.savefig(output_images_folder + '/' + operationId + "_kuukausittain.png")
+            plt.close()
